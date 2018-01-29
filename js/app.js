d91f421d84237af6bbd63d3f5b7638b87268c566
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-29 13:58:55 -0500

    Implementa SELECT dinámico para años, con rango de 20 años usando JavaScript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-  
+  /* Cotizador */
+const anio_max = new Date() .getFullYear(),           // Obtiene año actual   
+anio_min = anio_max - 20,                             // Obtiene año actual menos 20 años
+selectAnios = document .getElementById( 'anio' );     // Obtiene el SELECT de años en el DOM
+
+/* Recorre los años dentro del rango */
+for( let i = anio_max; i > anio_min; i-- ) {
+    let option = document .createElement( 'option' );       // Crea el elemento 'option'
+
+    option .value = i;                                      // Agrega la propiedad 'value' y su valor (año)
+    option .innerHTML = i;                                  // Agrega nombre a la opción
+    selectAnios .appendChild( option );                     // Despliega elemento en el DOM
+}    
