f442cd6735168a0f8382498a2edd878169b6570a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-29 14:32:24 -0500

    Obtener los valores de los campos seleccionados del Formulario

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
   /* Cotizador */
 const anio_max = new Date() .getFullYear(),           // Obtiene año actual   
 anio_min = anio_max - 20,                             // Obtiene año actual menos 20 años
-selectAnios = document .getElementById( 'anio' );     // Obtiene el SELECT de años en el DOM
+selectAnios = document .getElementById( 'anio' ),     // Obtiene el SELECT de años en el DOM
+formulario = document .getElementById( 'cotizar-seguro' );  // Obtiene el elemento FORM en el DOM
 
 /* Recorre los años dentro del rango */
 for( let i = anio_max; i > anio_min; i-- ) {
@@ -9,5 +10,22 @@ for( let i = anio_max; i > anio_min; i-- ) {
 
     option .value = i;                                      // Agrega la propiedad 'value' y su valor (año)
     option .innerHTML = i;                                  // Agrega nombre a la opción
-    selectAnios .appendChild( option );                     // Despliega elemento en el DOM
+    selectAnios .appendChild( option );                     // Despliega elemento en el DOM  
 }    
+
+/* EventListeners (Eventos que escuchan)  */
+formulario .addEventListener( 'submit', function( e ) {
+    const marca = document .getElementById( 'marca' ),                                  // Obtiene el SELECT de marca en el DOM 
+          marcaSeleccionada = marca .options[ marca .selectedIndex ] .value,            // Obtiene el valor del elemento SELECT de marca en el DOM 
+          anio = document .getElementById( 'anio' ),                                    // Obtiene el SELECT de anio en el DOM 
+          anioSeleccionado = anio .options[ anio .selectedIndex ] .value,               // Obtiene el valor del elemento SELECT de anio en el DOM
+          tipoSeguro = document .querySelector( 'input[name="tipo"]:checked' ) .value;  // Obtiene el valor del elemento RADIO (tipo) en el DOM 
+    
+    e .preventDefault();        // Detiene y previene que se ejecute el 'action' en el formulario
+
+    console .group( 'Selección realizada' );
+        console .log( 'Marca: ', marcaSeleccionada );      
+        console .log( 'Año: ',  anioSeleccionado );
+        console .log( 'Tipo seguro:', tipoSeguro );
+    console .groupEnd();
+});
